8d1e9419386611a50e701e4251bf56ec70c7d24a
infthi <infthi.inbox@gmail.com>
2013-06-24 19:40:43 +0400

    initial

---
+++ Flocal/src/main/java/ru/ith/android/flocal/activities/BoardListActivity.java
@@ -0,0 +1,16 @@
+package ru.ith.android.flocal.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import ru.ith.android.flocal.R;
+
+public class BoardListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_list);
+    }
+
+}
